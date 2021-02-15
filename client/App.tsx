e7b778d27aa7419b7b9a0d1cde5f033207b1e218
Bob Naydenov <bob.naydenov@hotmail.com>
2021-02-15 01:39:13 +0000

    Add login component and force users to log in before using the app

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { Login } from './components/Login';
 import { Search } from './components/Search';
 import { Result } from './components/Result';
 import { SearchResult } from './util/api';
 
 export default function App() {
+  const [initialising, setInitialising] = useState<boolean>(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
   const [data, setData] = useState<SearchResult | null>(null);
+
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+    setUser(user);
+    if (initialising) setInitialising(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+
+    return subscriber;
+  }, []);
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <Login />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
